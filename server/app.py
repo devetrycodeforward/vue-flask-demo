b34db8cf1ea63327e1219b893e01ce0e17330b4b
Philip Gonzalez <pgonzalez115@gmail.com>
2019-11-05 09:14:43 -0700

    Use postgres instead of sqlite3

--- server/app.py
+++ server/app.py
@@ -15,7 +15,7 @@ def create_app():
         static_folder = "./dist/static",
         template_folder = "./dist"
     )
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(project_dir, "vue-flask-todos.db"))
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://{user}:{pw}@{url}/{db}".format(user=os.environ["DB_USER"],pw=os.environ["DB_PASS"],url=os.environ["DB_URL"],db=os.environ["DB_NAME"])
     app.config['SQLALCHEMY_ECHO'] = True
     db.init_app(app)
     app.register_blueprint(todos_api)
